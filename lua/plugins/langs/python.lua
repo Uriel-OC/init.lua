5ba2d7935cdd37cae004b431c1ff17e2e6a7dd42
Uriel-OC <62571469+Uriel-OC@users.noreply.github.com>
2025-09-11 09:50:32 -0600

    Update python plugins config

--- lua/plugins/langs/python.lua
+++ lua/plugins/langs/python.lua
@@ -4,14 +4,7 @@ return {
   {
     "nvim-treesitter",
     opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, { "python" })
+      vim.list_extend(opts.parsers, { "python" })
     end
   },
-  {
-    "vim-slime",
-    keys = {
-      { "gzz", vim.cmd.SlimeSend, desc = "Send line to REPL", ft = "python" },
-      { "gz", ":SlimeSend<CR>", mode = "x", silent = true, desc = "Send region to REPL", ft = "python" },
-    },
-  }
 }
