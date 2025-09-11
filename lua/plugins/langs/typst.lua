0e486eee35dcaec12d8bed0458a412c75e56b9a4
Uriel-OC <62571469+Uriel-OC@users.noreply.github.com>
2025-09-11 09:52:21 -0600

    Update typst plugins config

--- lua/plugins/langs/typst.lua
+++ lua/plugins/langs/typst.lua
@@ -4,11 +4,11 @@ return {
   {
     "nvim-treesitter",
     opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, { "typst" })
+      vim.list_extend(opts.parsers, { "typst" })
     end
   },
   {
-    'chomosuke/typst-preview.nvim',
+    "chomosuke/typst-preview.nvim",
     version = '1.*',
     opts = {
       invert_colors = "always",
