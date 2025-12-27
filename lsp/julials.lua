b30ff39ca9c52231c9b4ab300fdfcd63bec60634
Uriel-OC <62571469+Uriel-OC@users.noreply.github.com>
2025-12-26 18:20:36 -0600

    Simplify Julia LSP config

--- lsp/julials.lua
+++ lsp/julials.lua
@@ -1,33 +1,18 @@
 local cmd = {
-  'julia',
-  "--project=~/.julia/environments/nvim-lspconfig",
-  '--startup-file=no',
-  '--history-file=no',
-  '-e',
+  "julia",
+  "--project=@lsp",
+  "--startup-file=no",
+  "--history-file=no",
+  "-e",
   [[
     using LanguageServer
-    depot_path = get(ENV, "JULIA_DEPOT_PATH", "")
-    project_path = let
-        dirname(something(
-            ## 1. Finds an explicitly set project (JULIA_PROJECT)
-            Base.load_path_expand((
-                p = get(ENV, "JULIA_PROJECT", nothing);
-                p === nothing ? nothing : isempty(p) ? nothing : p
-            )),
-            ## 2. Look for a Project.toml file in the current working directory,
-            ##    or parent directories, with $HOME as an upper boundary
-            Base.current_project(),
-            ## 3. First entry in the load path
-            get(Base.load_path(), 1, nothing),
-            ## 4. Fallback to default global environment,
-            ##    this is more or less unreachable
-            Base.load_path_expand("@v#.#"),
-        ))
-    end
-    @info "Running language server" VERSION pwd() project_path depot_path
+    const depot_path = get(ENV, "JULIA_DEPOT_PATH", "")
+    const project_path = Base.current_project() |> dirname
+    function (@main)(args)
     server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path, depot_path)
     server.runlinter = true
     run(server)
+    end
   ]],
 }
 
@@ -36,4 +21,5 @@ return {
   cmd = cmd,
   filetypes = { "julia" },
   root_markers = { "Project.toml" },
+  workspace_required = true
 }
