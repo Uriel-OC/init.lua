fc631b7ac706ee5646b3e7440e1547ecee61a6b5
Uriel-OC <62571469+Uriel-OC@users.noreply.github.com>
2025-12-26 18:07:53 -0600

    Remove keymap opts since Julia LSP takes care of providing Unicode symbols

--- after/ftplugin/julia.lua
+++ after/ftplugin/julia.lua
@@ -4,6 +4,3 @@ vim.wo[0][0].foldmethod = "expr"
 vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
 
 vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-
-vim.bo.keymap = "N2U"
-vim.bo.iminsert = 0
