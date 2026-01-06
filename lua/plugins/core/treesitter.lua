1f832673d91147c6e710381ee71d0cacf3696fb9
Uriel-OC <62571469+Uriel-OC@users.noreply.github.com>
2026-01-05 18:44:14 -0600

    Add nvim-treesitter-textobjects plugin

--- lua/plugins/core/treesitter.lua
+++ lua/plugins/core/treesitter.lua
@@ -76,4 +76,11 @@ return {
       line_numbers = false,
     },
   },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    init = function()
+      vim.g.no_plugin_maps = true
+    end
+  },
 }
