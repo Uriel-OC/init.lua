6ff9218381c5270f347399e952e4dd885ab10a0d
Uriel-OC <62571469+Uriel-OC@users.noreply.github.com>
2026-01-12 22:48:20 -0600

    Include more filetype plugins

---
+++ ftplugin/lua.lua
@@ -0,0 +1,8 @@
+vim.b.did_ftplugin = 1
+
+vim.bo.includeexpr = ""
+
+vim.bo.comments = ":---,:--" -- kekw
+vim.bo.commentstring = "-- %s"
+
+vim.bo.formatoptions = "r"
---
+++ ftplugin/python.lua
@@ -0,0 +1,5 @@
+vim.b.did_ftplugin = 1
+
+vim.bo.commentstring = "# %s"
+
+vim.opt.wildignore:append("*.pyc")
