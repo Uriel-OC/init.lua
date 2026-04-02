b7a68442fcf56e17b6f4a54c9a9a4dc2882b82af
Uriel-OC <62571469+Uriel-OC@users.noreply.github.com>
2026-04-02 08:09:36 -0600

    Disabling color inversion for Typst preview

--- lua/plugins/langs/typst.lua
+++ lua/plugins/langs/typst.lua
@@ -10,7 +10,6 @@ return {
     "chomosuke/typst-preview.nvim",
     version = '1.*',
     opts = {
-      invert_colors = "always",
       dependencies_bin = {
         tinymist = "tinymist",
         websocat = "websocat",
