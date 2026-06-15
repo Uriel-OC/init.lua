cc68613e543712f30f1ceb1f7208a5c623a87629
Uriel-OC <62571469+Uriel-OC@users.noreply.github.com>
2026-06-15 10:30:24 -0600

    Replace LanguageServer.jl LSP with JETLS

---
+++ lsp/jetls.lua
@@ -0,0 +1,13 @@
+---@type vim.lsp.Config
+return {
+  cmd = { "jetls", "serve" },
+  filetypes = { "julia" },
+  root_markers = { "Project.toml" },
+  init_options = {
+    analysis_overrides = {
+      { path = "src/**/*.jl" },
+      { path = "test/**/*.jl" },
+    }
+  },
+  workspace_required = true
+}
