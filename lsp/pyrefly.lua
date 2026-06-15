a9e3a565bf236c8378bb24534598552aa1045c8a
Uriel-OC <62571469+Uriel-OC@users.noreply.github.com>
2026-06-15 10:29:23 -0600

    Replase BasedPyright LSP with Pyrefly

---
+++ lsp/pyrefly.lua
@@ -0,0 +1,11 @@
+---@type vim.lsp.Config
+return {
+  cmd = { "pyrefly", "lsp" },
+  filetypes = { "python" },
+  root_markers = { "pyproject.toml" },
+  on_exit = function(code, _, _)
+    vim.schedule(function()
+      vim.notify("Closing Pyrefly LSP exited with code: " .. code, vim.log.levels.INFO)
+    end)
+  end,
+}
