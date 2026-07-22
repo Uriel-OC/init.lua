733e17c1ae79590cfc3798d734adb148f04a1e3d
Uriel-OC <62571469+Uriel-OC@users.noreply.github.com>
2026-07-22 16:49:57 -0600

    Changing "TexLab" config
    The following changes were made:
    - Skim was replaced by Sioyek as the PDF reader used in forward search.
    - Deletion of `:LspTexlabClean` user command. I hardly ever used it.
    - Changed how requests are sent to the server, now it doesn't use the
      depecrated function.
    - Little code refactor.

--- lsp/texlab.lua
+++ lsp/texlab.lua
@@ -1,3 +1,11 @@
+local cancel_group = vim.api.nvim_create_augroup("CancelBuild", { clear = true })
+local status_codes = {
+  [0] = "Success",
+  [1] = "Error",
+  [2] = "Failure",
+  [3] = "Cancelled",
+}
+
 ---@type vim.lsp.Config
 return {
   cmd = { "texlab" },
@@ -12,10 +20,21 @@ return {
         pdfDirectory = "output",
       },
       forwardSearch = {
-        executable = "displayline",
-        args = { "-r", "-b", "-n", "%l", "%p", "%f" },
+        executable = "/Applications/sioyek.app/Contents/MacOS/sioyek",
+        args = {
+          "--reuse-window",
+          "--execute-command",
+          "turn_on_synctex",
+          "--forward-search-file",
+          "%f",
+          "--forward-search-line",
+          "%l",
+          "%p",
+        },
       },
       chktex = { onEdit = true },
+      latexFormatter = "tex-fmt",
+      hover = { symbols = "glyph" },
       experimental = { followPackageLinks = true },
     }
   },
@@ -24,61 +43,28 @@ return {
       local win = vim.api.nvim_get_current_win()
       local params = vim.lsp.util.make_position_params(win, client.offset_encoding)
       ---@diagnostic disable-next-line: param-type-mismatch
-      client.request("textDocument/build", params, function(err, result)
+      client:request("textDocument/build", params, function(err, result)
         if err then
           error(tostring(err))
         end
-        local texlab_build_status = {
-          [0] = "Success",
-          [1] = "Error",
-          [2] = "Failure",
-          [3] = "Cancelled",
-        }
-        vim.notify("Build " .. texlab_build_status[result.status], vim.log.levels.INFO)
+        vim.notify("Build " .. status_codes[result.status], vim.log.levels.INFO)
       end)
     end, { silent = true, desc = "Compile PDF", buffer = bufnr })
     vim.keymap.set("n", "<localleader>lv", function()
       local win = vim.api.nvim_get_current_win()
       local params = vim.lsp.util.make_position_params(win, client.offset_encoding)
       ---@diagnostic disable-next-line: param-type-mismatch
-      client.request("textDocument/forwardSearch", params, function(err, result)
+      client:request("textDocument/forwardSearch", params, function(err, result)
         if err then
           error(tostring(err))
         end
-        local texlab_forward_status = {
-          [0] = "Success",
-          [1] = "Error",
-          [2] = "Failure",
-          [3] = "Unconfigured",
-        }
-        vim.notify("Search " .. texlab_forward_status[result.status], vim.log.levels.INFO)
+        vim.notify("Search " .. status_codes[result.status], vim.log.levels.INFO)
       end)
     end, { silent = true, desc = "Perform forward search", buffer = bufnr })
 
-    vim.api.nvim_buf_create_user_command(bufnr, "LspTexlabClean", function()
-      client:exec_cmd({
-        title = "Clean auxiliary",
-        command = "texlab.cleanAuxiliary",
-        arguments = { { uri = vim.uri_from_bufnr(bufnr) } },
-      }, { bufnr = bufnr }, function(err, _)
-        if err then
-          vim.notify("Failed to clean auxiliary files", vim.log.levels.ERROR)
-        end
-      end)
-      client:exec_cmd({
-        title = "Clean artifacts",
-        command = "texlab.cleanArtifacts",
-        arguments = { { uri = vim.uri_from_bufnr(bufnr) } },
-      }, { bufnr = bufnr }, function(err, _)
-        if err then
-          vim.notify("Failed to clean artifacts", vim.log.levels.ERROR)
-        end
-      end)
-    end, { desc = "Clean auxiliary files and artifacts" })
-
     vim.api.nvim_create_autocmd("VimLeavePre", {
       desc = "Cancel build gracefully before quit",
-      group = vim.api.nvim_create_augroup("CancelBuild", { clear = true }),
+      group = cancel_group,
       buffer = bufnr,
       callback = function(args)
         client:exec_cmd({
